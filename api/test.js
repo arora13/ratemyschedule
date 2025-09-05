d15580b0793c5afa74ba35ad4a51c5ac14d9c450
Arjun Arora <arjunarora.dev@gmail.com>
2025-09-04 21:58:31 -0700

    Simplify test API function to use ES6 export

--- api/test.js
+++ api/test.js
@@ -1,12 +1,3 @@
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
+export default function handler(req, res) {
   res.status(200).json({ message: 'API is working!', timestamp: new Date().toISOString() });
-};
+}
